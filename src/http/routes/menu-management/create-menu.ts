fe59b84746b5302598407c08d46c22d5423de11a
alaanvv <valealan84@gmail.com>
2024-07-16 14:37:18 -0300

    fix: create-menu needs auth and optional stuff

--- src/http/routes/menu-management/create-menu.ts
+++ src/http/routes/menu-management/create-menu.ts
@@ -10,13 +10,13 @@ export default async function(app: FastifyInstance) {
       username: z.string(),
       password: z.string(),
       name:     z.string(),
-      phone:    z.string(),
-      whatsapp: z.string(),
-      address:  z.string()
+      phone:    z.optional(z.string()),
+      whatsapp: z.optional(z.string()),
+      address:  z.optional(z.string())
     })
     const data = bodySchema.parse(req.body)
 
-    if (!is_trusted_ip(req)) throw new ForbiddenError('No privileges.')
+    if (!(await is_trusted_ip(req))) throw new ForbiddenError('No privileges.')
 
     const menu = await prisma.menu.create({ data })
 

c24f3b67191b2cf59d9fae2f73b32b5616825d7a
alaanvv <valealan84@gmail.com>
2024-07-16 14:37:32 -0300

    feat: delete is made by admin

--- src/http/routes/menu-management/delete-menu.ts
+++ src/http/routes/menu-management/delete-menu.ts
@@ -1,6 +1,6 @@
 import { BadRequestError, ForbiddenError } from '../../errors'
 import { FastifyInstance } from 'fastify'
-import { get_auth } from '../../utils/auth'
+import { is_trusted_ip } from '../../utils/auth'
 import { prisma } from '../../../lib/prisma'
 import { z } from 'zod'
 
@@ -9,7 +9,7 @@ export default async function(app: FastifyInstance) {
     const paramSchema = z.object({ id: z.string().cuid() })
     const { id } = paramSchema.parse(req.params)
 
-    if (!(await get_auth(req, id))) throw new ForbiddenError('No privileges.')
+    if (!(await is_trusted_ip(req))) throw new ForbiddenError('No privileges.')
 
     try   { await prisma.menu.delete({ where: { id } }) }
     catch { throw new BadRequestError('Menu not found.') }

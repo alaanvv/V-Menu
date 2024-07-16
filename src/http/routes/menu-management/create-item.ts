a05d28720187db5b4cb644252d7b993b452ea45f
alaanvv <valealan84@gmail.com>
2024-07-15 21:03:42 -0300

    feat: made description not required

--- src/http/routes/menu-management/create-item.ts
+++ src/http/routes/menu-management/create-item.ts
@@ -7,7 +7,7 @@ export default async function(app: FastifyInstance) {
   app.post('/item/:id', async (req, res) => {
     const bodySchema  = z.object({
       name:           z.string(),
-      description:    z.string(),
+      description:    z.optional(z.string()),
       price_in_cents: z.number().int().min(1),
     })
     const paramSchema = z.object({ id: z.string().cuid() })
@@ -18,7 +18,14 @@ export default async function(app: FastifyInstance) {
     if (!await prisma.subcategory.findUnique({ where: { id } }))
       throw new NotFoundError('Subcategory not found.')
 
-    const item = await prisma.item.create({ data: { ...data, subcategory_id: id } })
+    const max_pos = await prisma.item.aggregate({
+      where: { subcategory_id: id },
+      _max: { pos: true }
+    })
+
+    const pos = (max_pos._max.pos || 0) + 1
+
+    const item = await prisma.item.create({ data: { ...data, subcategory_id: id, pos } })
 
     return res.status(201).send({ item })
   })
